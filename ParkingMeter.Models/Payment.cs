8dc84c917d53a12f29dfb69e7abf47a09e5c65d0
Ömer Kutlay <omer.kutlay@outlook.com>
2025-01-06 19:44:13 +0300

    payment modeli oluşturuldu

---
+++ ParkingMeter.Models/Payment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMeter.Models
+{
+    public class Payment:BaseModel
+    {
+        public decimal Amount { get; set; }
+        public bool IsPaid { get; set; }= false;
+        public int VehicleId { get; set; }
+        public virtual Vehicle Vehicle { get; set; }
+    }
+}
