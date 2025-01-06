924be2435cbead3ebb6dd0cd06806aefad4aa1d7
Ömer Kutlay <omer.kutlay@outlook.com>
2025-01-06 17:08:34 +0300

    BaseModel oluşturuldu

---
+++ ParkingMeter.Models/BaseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMeter.Models
+{
+    public class BaseModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; }
+
+
+    }
+}
