18b519c23973ccb15e53244839d94cae1f0507ce
Ömer Kutlay <omer.kutlay@outlook.com>
2025-01-06 19:44:48 +0300

    ParkSlot modelinde düzenlemeler yapıldı

--- ParkingMeter.Models/ParkSlot.cs
+++ ParkingMeter.Models/ParkSlot.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ParkingMeter.Models
 {
-    public class ParkSlot:BaseModel
+    public class ParkSlot : BaseModel
     {
-        public string Row {  get; set; }
+        public string Row { get; set; }
         public string Column { get; set; }
         public bool IsOccupied { get; set; }
-        public int VehicleId { get; set; }
-       // public virtual Vehicle Vehicle { get; set; }
+        [ForeignKey("Vehicle")]
+        public int? VehicleId { get; set; }
+        public virtual Vehicle? Vehicle { get; set; }
+        public int StatusId { get; set; }
+        public virtual Status Status { get; set; }
     }
 }
