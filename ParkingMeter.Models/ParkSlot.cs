c2b48be8aa637b6a9de1882e621c823670e86689
Ömer Kutlay <omer.kutlay@outlook.com>
2025-01-06 17:08:53 +0300

    ParkSlot modeli oluşturuldu

---
+++ ParkingMeter.Models/ParkSlot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMeter.Models
+{
+    public class ParkSlot:BaseModel
+    {
+        public string Row {  get; set; }
+        public string Column { get; set; }
+        public bool IsOccupied { get; set; }
+        public int VehicleId { get; set; }
+       // public virtual Vehicle Vehicle { get; set; }
+    }
+}
