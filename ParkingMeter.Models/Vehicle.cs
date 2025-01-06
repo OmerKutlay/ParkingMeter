6ca10dd2d03ff77d72cc701066a932cebb16ca0e
Ömer Kutlay <omer.kutlay@outlook.com>
2025-01-06 19:44:01 +0300

    Vehicle modeli oluşturuldu

---
+++ ParkingMeter.Models/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMeter.Models
+{
+    public class Vehicle : BaseModel
+    {
+        public string Plate { get; set; }
+        public string VehicleType { get; set; } = "Otomobil";
+        public string? ContactNumber { get; set; }
+        public bool IsSubscribed { get; set; } = false;
+        public DateTime EntryTime { get; set; }
+        public DateTime? ExitTime { get; set; }
+        public int? ParkSlotId { get; set; }
+        public virtual ParkSlot? ParkSlot { get; set; }
+        [ForeignKey("Payment")]
+        public int? PaymentId { get; set; }
+        public virtual Payment? Payment { get; set; }
+
+    }
+}
