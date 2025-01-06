5355327a6333e12cef0e8f06b5d28c2f847455ed
Ömer Kutlay <omer.kutlay@outlook.com>
2025-01-06 17:10:35 +0300

    ApplicationDbContext sınıfı oluşturuldu

---
+++ ParkingMeter.Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkingMeter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMeter.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+
+        }
+
+        public virtual DbSet<ParkSlot> ParkSlots { get; set; }
+    }
+}
