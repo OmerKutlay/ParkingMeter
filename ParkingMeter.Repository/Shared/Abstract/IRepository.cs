24b85a7a832488f3e2f823bb752ee6ab4c6ed64b
Ömer Kutlay <omer.kutlay@outlook.com>
2025-01-08 19:39:17 +0300

    IRepository interface'inin yapısı belirlendi

---
+++ ParkingMeter.Repository/Shared/Abstract/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMeter.Repository.Shared.Abstract
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> GetAll(Expression<Func<T, bool>> predicate);
+        T GetById(int id);
+        T Add(T entity);
+        T Update(T entity);
+        void Delete(int id);
+        List<T> AddRange(List<T> entities);
+        T GetFirstOrDefault(Expression<Func<T, bool>> predicate);
+        void Save();
+    }
+}
