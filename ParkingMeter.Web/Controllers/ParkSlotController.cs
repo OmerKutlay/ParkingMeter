9fe7a0faac131c19eb6d930dcc0af8f042b06ab5
Ömer Kutlay <omer.kutlay@outlook.com>
2025-01-09 14:19:05 +0300

    ParkSlot controller dosyası oluşturuldu

---
+++ ParkingMeter.Web/Controllers/ParkSlotController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ParkingMeter.Web.Controllers
+{
+    public class ParkSlotController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
