66d066251303e5033bedbdc228f572dfe9bedf12
Ömer Kutlay <omer.kutlay@outlook.com>
2025-01-09 15:30:14 +0300

    Business serviclerin concrete class dosyaları oluşturuldu

---
+++ ParkingMeter.Business/Concrete/VehicleService.cs
@@ -0,0 +1,46 @@
+﻿using ParkingMeter.Business.Abstract;
+using ParkingMeter.Models;
+using ParkingMeter.Repository.Shared.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMeter.Business.Concrete
+{
+    public class VehicleService: IVehicleService
+    {
+        private readonly IRepository<Vehicle> _vehicleRepository;
+
+        public VehicleService(IRepository<Vehicle> vehicleRepository)
+        {
+            _vehicleRepository = vehicleRepository;
+        }
+
+        public Vehicle Add(Vehicle vehicle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int vehicleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Vehicle> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vehicle GetById(int vehicleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vehicle Update(Vehicle vehicle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
