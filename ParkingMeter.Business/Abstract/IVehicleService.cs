a264264c6470b4c06a861bf33e7093459bc302b4
Ömer Kutlay <omer.kutlay@outlook.com>
2025-01-09 15:29:36 +0300

    VehicleService interface'i oluşturuldu

---
+++ ParkingMeter.Business/Abstract/IVehicleService.cs
@@ -0,0 +1,18 @@
+﻿using ParkingMeter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMeter.Business.Abstract
+{
+    public interface IVehicleService
+    {
+        Vehicle Add(Vehicle vehicle);
+        Vehicle Update(Vehicle vehicle);
+        bool Delete(int vehicleId);
+        IQueryable<Vehicle> GetAll();
+        Vehicle GetById(int vehicleId);
+    }
+}
