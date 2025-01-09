aa5725e5179d8ffaad16c831063dfbc174aa684f
Ömer Kutlay <omer.kutlay@outlook.com>
2025-01-09 15:28:46 +0300

    ParkSlotServive interface'i oluşturuldu

---
+++ ParkingMeter.Business/Abstract/IParkSlotService.cs
@@ -0,0 +1,19 @@
+﻿using ParkingMeter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMeter.Business.Abstract
+{
+    public interface IParkSlotService
+    {
+        ParkSlot Add(ParkSlot ParkSlot);
+        ParkSlot Update(ParkSlot ParkSlot);
+        ParkSlot GetById(int id);
+        bool Delete(int ParkSlotId);
+        IQueryable<ParkSlot> GetAll();
+        IQueryable<ParkSlot> GetFreeParkSlots();
+    }
+}
