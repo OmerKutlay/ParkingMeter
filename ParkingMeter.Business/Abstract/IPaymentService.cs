febf83bc1174a2c5957365baafa0af59e5da9d3e
Ömer Kutlay <omer.kutlay@outlook.com>
2025-01-09 15:29:04 +0300

    PaymentService interface'i oluşturuldu

---
+++ ParkingMeter.Business/Abstract/IPaymentService.cs
@@ -0,0 +1,20 @@
+﻿using ParkingMeter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMeter.Business.Abstract
+{
+    public interface IPaymentService
+    {
+        Payment Add (Payment payment);
+        Payment Update (Payment payment);
+        Payment GetById (int id);
+        List<Payment> GetAllByVehicleId (int vehicleId);
+        bool Delete(int PaymentId);
+        IQueryable<Payment> GetAll();
+
+    }
+}
