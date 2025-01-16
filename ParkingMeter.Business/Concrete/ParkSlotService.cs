8b407ec461631ffb9f74c9441aa63c694bafe466
Ömer Kutlay <omer.kutlay@outlook.com>
2025-01-16 16:27:24 +0300

    ParkSlot Service için business katmanındaki concrete metotları yazıldı

--- ParkingMeter.Business/Concrete/ParkSlotService.cs
+++ ParkingMeter.Business/Concrete/ParkSlotService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ParkingMeter.Business.Abstract;
+using ParkingMeter.Models;
+using ParkingMeter.Repository.Shared.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,43 @@ using System.Threading.Tasks;
 
 namespace ParkingMeter.Business.Concrete
 {
-    internal class ParkSlotService
+    public class ParkSlotService : IParkSlotService
     {
+        private readonly IRepository<ParkSlot> _parkSlotRepository;
+
+        public ParkSlotService(IRepository<ParkSlot> parkSlotRepository)
+        {
+            _parkSlotRepository = parkSlotRepository;
+        }
+        public ParkSlot Add(ParkSlot ParkSlot)
+        {
+            return _parkSlotRepository.Add(ParkSlot);
+        }
+
+        public bool Delete(int ParkSlotId)
+        {
+            _parkSlotRepository.Delete(ParkSlotId);
+            return true;
+        }
+
+        public IQueryable<ParkSlot> GetAll()
+        {
+            return _parkSlotRepository.GetAll();
+        }
+
+        public ParkSlot GetById(int id)
+        {
+            return _parkSlotRepository.GetById(id);
+        }
+
+        public IQueryable<ParkSlot> GetFreeParkSlots()
+        {
+            return _parkSlotRepository.GetAll(ps => ps.IsDeleted == false).Where(ps => ps.IsOccupied == false);
+        }
+
+        public ParkSlot Update(ParkSlot ParkSlot)
+        {
+          return _parkSlotRepository.Update(ParkSlot);
+        }
     }
 }
