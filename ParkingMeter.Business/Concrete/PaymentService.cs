0de66ebc4afdf7b077a67344f0c48d9d046d3c27
Ömer Kutlay <omer.kutlay@outlook.com>
2025-01-16 16:27:54 +0300

    Payment Service için business concrete metotları yazıldı

--- ParkingMeter.Business/Concrete/PaymentService.cs
+++ ParkingMeter.Business/Concrete/PaymentService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ParkingMeter.Business.Abstract;
+using ParkingMeter.Models;
+using ParkingMeter.Repository.Shared.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,44 @@ using System.Threading.Tasks;
 
 namespace ParkingMeter.Business.Concrete
 {
-    internal class PaymentService
+    public class PaymentService : IPaymentService
     {
+        private readonly IRepository<Payment> _paymentRepository;
+
+        public PaymentService(IRepository<Payment> paymentRepository)
+        {
+            _paymentRepository = paymentRepository;
+        }
+
+        public Payment Add(Payment payment)
+        {
+            return _paymentRepository.Add(payment);
+        }
+
+        public bool Delete(int PaymentId)
+        {
+            _paymentRepository.Delete(PaymentId);
+            return true;
+        }
+
+        public IQueryable<Payment> GetAll()
+        {
+           return _paymentRepository.GetAll();
+        }
+
+        public List<Payment> GetAllByVehicleId(int vehicleId)
+        {
+           return _paymentRepository.GetAll(p => p.VehicleId == vehicleId).ToList();
+        }
+
+        public Payment GetById(int id)
+        {
+            return _paymentRepository.GetById(id);
+        }
+
+        public Payment Update(Payment payment)
+        {
+            return _paymentRepository.Update(payment);
+        }
     }
 }
