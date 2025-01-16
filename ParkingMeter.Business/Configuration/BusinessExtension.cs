6b1c2c3d4036e40e80a84f3dc928355d288fb342
Ömer Kutlay <omer.kutlay@outlook.com>
2025-01-16 16:28:19 +0300

    Business Extension class'ı oluşturuldu

---
+++ ParkingMeter.Business/Configuration/BusinessExtension.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ParkingMeter.Business.Abstract;
+using ParkingMeter.Business.Concrete;
+using ParkingMeter.Repository.Shared.Abstract;
+using ParkingMeter.Repository.Shared.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMeter.Business.Configuration
+{
+    public static class BusinessExtension
+    {
+        public static void BusinessDI(this IServiceCollection services)
+        {
+            services.AddScoped<IParkSlotService, ParkSlotService>();
+            services.AddScoped<IPaymentService, PaymentService>();
+            services.AddScoped<IVehicleService, VehicleService>();
+        }
+
+        public static void RepositoryDI(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        }
+    }
+}
