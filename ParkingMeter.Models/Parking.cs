060f6ba485c276173813d3c75213670342c26719
Ömer Kutlay <omer.kutlay@outlook.com>
2025-01-20 18:14:59 +0300

    Parking Modeli oluşturuldu

---
+++ ParkingMeter.Models/Parking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMeter.Models
+{
+    public class Parking:BaseModel
+    {
+        public int ParkSlotId { get; set; }
+        public virtual ParkSlot ParkSlot { get; set; }
+        public ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
+        public ICollection<Payment> Payments { get; set; }= new List<Payment>();
+    }
+}
