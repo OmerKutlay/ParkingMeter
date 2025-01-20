d2de6a9219bd04af572896895debc9b15943ddff
Ömer Kutlay <omer.kutlay@outlook.com>
2025-01-20 19:10:31 +0300

    IParkingService interface'i oluşturuldu

---
+++ ParkingMeter.Business/Abstract/IParkingService.cs
@@ -0,0 +1,18 @@
+﻿using ParkingMeter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingMeter.Business.Abstract
+{
+    public interface IParkingService
+    {
+        Parking Add(Parking parking);
+        Parking Update(Parking parking);
+        bool Delete(int parkingId);
+        IQueryable<Parking> GetAll();
+        Parking GetById(int parkingId);
+    }
+}
